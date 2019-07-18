d3eecf24ce7fc6893dc3668f221e5615e257b074
a-yasinsky <anatoly@yasinsky.pro>
2019-07-18 16:40:28 +0600

    lib import and data load

---
+++ loadData.py
@@ -0,0 +1,7 @@
+# Importing pandas and numpy
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reading the csv file into a pandas DataFrame
+data = pd.read_csv('newsData.csv', encoding = "ISO-8859-1")
