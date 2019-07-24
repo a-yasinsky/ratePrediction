e0282a682498eab565b541bc8ced4e5433cd1fe2
a-yasinsky <anatoly@yasinsky.pro>
2019-07-24 15:12:33 +0600

    tokenized

--- loadData.py
+++ loadData.py
@@ -3,18 +3,21 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from nltk.tokenize import TreebankWordTokenizer
+from nltk.stem import WordNetLemmatizer
 import re
 
 # Reading the csv file into a pandas DataFrame
 newsData = pd.read_csv('news_rates.csv', encoding = "ISO-8859-1")
 
 tokenizer = TreebankWordTokenizer()
+stemmer = WordNetLemmatizer()
 
 def data_cleaner(text):
 	letters_only = re.sub("[^a-zA-Z]", " ", text)
 	lower_case = letters_only.lower()
-	words = tokenizer.tokenize(lower_case)
-	return (" ".join(words)).strip()
+	tokens = tokenizer.tokenize(lower_case)
+	base_words = [stemmer.lemmatize(token) for token in tokens]
+	return base_words
 
 testing = newsData.text[:10]
 test_result = []
