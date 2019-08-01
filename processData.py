e708bd232bc0ab72f9c6ef97dba40fe951147b2f
a-yasinsky <anatoly@yasinsky.pro>
2019-08-01 11:29:30 +0600

    add stop words

--- processData.py
+++ processData.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import text
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
@@ -18,9 +19,15 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.02, random_
 print(len(x_train), len(x_test))
 print(len(y_train), len(y_test))
 
+my_stop_words = text.ENGLISH_STOP_WORDS
+
 def tfidf_features(x_train, x_test):
-    
+    #init tfidf with frequency limits for stop words
 	tfidf = TfidfVectorizer(min_df=2, max_df=0.5, ngram_range=(1, 3))
+	
+	#init tfidf with stop words from sklearn
+	#tfidf = TfidfVectorizer(stop_words=my_stop_words, ngram_range=(1, 3))
+	
 	#tfidf features form clean text
 	train_features = tfidf.fit_transform(x_train)
 	test_features = tfidf.transform(x_test)
@@ -46,3 +53,5 @@ print("Mean squared error: %.2f"
       % mean_squared_error(y_test, y_pred))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(y_test, y_pred))
+
+print(list(zip(y_test,y_pred))[:20])
